224ac9d2289689cf0d7b45825ea7bbba52d6be82
victorianawire <34067099+victorianawire@users.noreply.github.com>
2021-08-02 03:46:16 +0300

    changes to js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var price, crust_price, topping_price;
 let total = 0;
 
-function Getpizza(name, size, crust, topping, total) {
+function getPizza(name, size, crust, topping, total) {
     this.name = name;
     this.size = size;
     this.crust = crust;
@@ -12,11 +12,7 @@ function Getpizza(name, size, crust, topping, total) {
 
 // proceed button
 $(document).ready(function() {
-    // $("button.proceed").click(function(){
-    //   $("button.proceed").hide();
-    //   $("#information").hide();
-    //   $("div.choise").slideDown(1000);
-    // });
+
     $("button.proceed").click(function(event) {
         let pname = $(".name option:selected").val();
         let psize = $("#size option:selected").val();
